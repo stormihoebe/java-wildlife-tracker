1a3315696f7f3512be467b73a9d2814540332531
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-07 09:43:07 -0700

    add save and update method to ranger

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -17,4 +17,24 @@ public Ranger(String name) {
   public int getId() {
     return id;
   }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()){
+      String sql = "INSERT INTO rangers (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public void updateName(String name){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE rangers SET name = :name WHERE id=:id";
+      con.createQuery(sql)
+      .addParameter("name", name)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
 }

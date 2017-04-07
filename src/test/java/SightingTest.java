4f33778d8349dd462f5be049444c6a16fb81fa4a
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-07 10:24:13 -0700

    refactored sightings test to include changes

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.sql.Timestamp;
+
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -20,54 +22,54 @@ public class SightingTest {
     assertEquals(true, testSighting instanceof Sighting);
   }
 
-  // @Test
-  // public void equals_returnsTrueIfLocationAndDescriptionAreSame_true() {
-  //   Animal testAnimal = new Animal("Deer");
-  //   testAnimal.save();
-  //   Sighting testSighting = new Sighting(testAnimal.getId(), "45.472428, -121.946466", 2);
-  //   Sighting anotherSighting = new Sighting(testAnimal.getId(), "45.472428, -121.946466", 2);
-  //   assertTrue(testSighting.equals(anotherSighting));
-  // }
-  //
-  // @Test
-  // public void save_insertsObjectIntoDatabase_Sighting() {
-  //   Animal testAnimal = new Animal("Deer");
-  //   testAnimal.save();
-  //   Sighting testSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", 2);
-  //   testSighting.save();
-  //   assertEquals(true, Sighting.all().get(0).equals(testSighting));
-  // }
-  //
-  // @Test
-  // public void all_returnsAllInstancesOfSighting_true() {
-  //   Animal testAnimal = new Animal("Deer");
-  //   testAnimal.save();
-  //   Sighting testSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", 2);
-  //   testSighting.save();
-  //   Animal secondTestAnimal = new Animal("Badger");
-  //   secondTestAnimal.save();
-  //   Sighting secondTestSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", 3);
-  //   secondTestSighting.save();
-  //   assertEquals(true, Sighting.all().get(0).equals(testSighting));
-  //   assertEquals(true, Sighting.all().get(1).equals(secondTestSighting));
-  // }
-  //
-  // @Test
-  // public void find_returnsSightingWithSameId_secondSighting() {
-  //   Animal testAnimal = new Animal("Deer");
-  //   testAnimal.save();
-  //   Sighting testSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", 2);
-  //   testSighting.save();
-  //   Animal secondTestAnimal = new Animal("Badger");
-  //   secondTestAnimal.save();
-  //   Sighting secondTestSighting = new Sighting (testAnimal.getId(), "45.472428, -121.946466", 3);
-  //   secondTestSighting.save();
-  //   assertEquals(Sighting.find(secondTestSighting.getId()), secondTestSighting);
-  // }
+  @Test
+  public void equals_returnsTrueIfLocationAndDescriptionAreSame_true() {
+    NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Deer");
+    testNonEndangeredAnimal.save();
+    Sighting testSighting = new Sighting(testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 2);
+    Sighting anotherSighting = new Sighting(testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 2);
+    assertTrue(testSighting.equals(anotherSighting));
+  }
 
-  // @Test
-  // public void find_returnsNullWhenNoAnimalFound_null() {
-  //   assertTrue(Animal.find(999) == null);
-  // }
+  @Test
+  public void save_insertsObjectIntoDatabase_Sighting() {
+    NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Deer");
+    testNonEndangeredAnimal.save();
+    Sighting testSighting = new Sighting (testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 2);
+    testSighting.save();
+    assertEquals(true, Sighting.all().get(0).equals(testSighting));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfSighting_true() {
+    NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Deer");
+    testNonEndangeredAnimal.save();
+    Sighting testSighting = new Sighting (testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 2);
+    testSighting.save();
+    NonEndangeredAnimal secondTestNonEndangeredAnimal = new NonEndangeredAnimal("Badger");
+    secondTestNonEndangeredAnimal.save();
+    Sighting secondTestSighting = new Sighting (testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 3);
+    secondTestSighting.save();
+    assertEquals(true, Sighting.all().get(0).equals(testSighting));
+    assertEquals(true, Sighting.all().get(1).equals(secondTestSighting));
+  }
+
+  @Test
+  public void find_returnsSightingWithSameId_secondSighting() {
+    NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Deer");
+    testNonEndangeredAnimal.save();
+    Sighting testSighting = new Sighting (testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 2);
+    testSighting.save();
+    NonEndangeredAnimal secondTestNonEndangeredAnimal = new NonEndangeredAnimal("Badger");
+    secondTestNonEndangeredAnimal.save();
+    Sighting secondTestSighting = new Sighting (testNonEndangeredAnimal.getId(), "45.472428, -121.946466", 3);
+    secondTestSighting.save();
+    assertEquals(Sighting.find(secondTestSighting.getId()), secondTestSighting);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoNonEndangeredAnimalFound_null() {
+    assertTrue(NonEndangeredAnimal.find(999) == null);
+  }
 
 }
